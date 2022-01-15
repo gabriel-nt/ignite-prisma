fd8df31fafdc5861a7c6207400b3007b89efc6b2
gabriel-nt <gabrielnt.dev@gmail.com>
2022-01-15 18:14:26 -0300

    Added authenticate client feature

---
+++ src/modules/account/authenticateClient/AuthenticateClientController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { AuthenticateClientUseCase } from './AuthenticateClientUseCase';
+
+export class AuthenticateClientController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const authenticateClientUseCase = new AuthenticateClientUseCase();
+
+    const result = await authenticateClientUseCase.execute({
+      password,
+      username,
+    });
+
+    return response.json(result);
+  }
+}
