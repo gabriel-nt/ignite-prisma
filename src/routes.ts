0e6239d2738fb55a1d2233077bcfacd98a787957
gabriel-nt <gabrielnt.dev@gmail.com>
2022-01-16 09:40:37 -0300

    Adding create delivery feature

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCase/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
@@ -12,6 +13,8 @@ const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
 
+const deliveryController = new CreateDeliveryController();
+
 routes.post('/client', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
 
@@ -20,5 +23,7 @@ routes.post(
   '/deliveryman/authenticate',
   authenticateDeliverymanController.handle
 );
+
+routes.post('/delivery', deliveryController.handle);
 
 export { routes };
